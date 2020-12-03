94fcea4a0262f4fd73363be541748f115e3233a9
saurabhsingh-cedcoss <74813543+saurabhsingh-cedcoss@users.noreply.github.com>
2020-12-03 11:58:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
 
-
+// ///////////////////////functon to call all the functions repeatedly on 10 millisecond///////////////////////////////////////
 
 setInterval(() => {
-    second();
-    minute();
-    hour();
+    var date = new Date();
+    var min = date.getMinutes();
+    var sec = date.getSeconds() + (date.getMilliseconds() / 1000);
+    var hour = date.getHours();
+    second(sec);
+    minute(min);
+    hours(hour, min);
 
 }, 10);
 
-function second() {
-    var date = new Date();
-    var sec= date.getSeconds()+(date.getMilliseconds()/1000);
+// function to transform seconds needle
+function second(sec) {
     var seconds = (6 * sec);
-    console.log(sec);
     var img = document.getElementById('second');
     img.style.transform = `rotate(${seconds}deg)`;
-
 }
-function minute() {
+// function to transform minute needle
+function minute(min) {
     var date = new Date();
-    var min = date.getMinutes();
     var rotation1 = 6 * min;
-    console.log(min);
     var img1 = document.getElementById('minute');
-
     img1.style.transform = `rotate(${rotation1}deg)`;
 }
-function hour() {
+// function to transform hours needle
+function hours(hour, min) {
     var date = new Date();
     var min = date.getMinutes();
-    var hour = date.getHours();
     var rotation2 = (hour * (30)) + (min / 2);
-    console.log(rotation2);
-    console.log(`.n${hour}`);
     var img1 = document.getElementById('hour');
     document.getElementById('show').innerHTML = hour + ":" + min;
-    // document.querySelector(`.n${hour}`).style.backgroundColor="rgb(124, 225, 65)";
-    // document.querySelector(`.n${hour}`).style.height="15%";
     img1.style.transform = `rotate(${rotation2}deg)`;
 }
-function stopwatch(){
-    document.querySelector('.clock').style.display="none";
-}
+
